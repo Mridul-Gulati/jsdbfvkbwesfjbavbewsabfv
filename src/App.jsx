19771af49294f23e9efe9db0376d5ac02a68e9de
mridulgulati18 <mridulgulati18@gmail.com>
2024-08-26 20:35:24 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { AuthProvider } from './AuthContext'
 import Hero from './components/custom/Hero'
 
 function App() {
@@ -10,9 +7,7 @@ function App() {
 
   return (
     <>
-      <AuthProvider>
-        <Hero />
-      </AuthProvider>
+      <Hero />
     </>
   )
 }
